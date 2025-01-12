b16281aa727ad34763e4c6ba328864d9287fe93a
zshcole <colburnsanders@icloud.com>
2025-01-12 14:29:26 -0800

    dynamic routes

---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,19 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Settings() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text>Settings</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+})
